3a5354d92cdc284f9cf7f522d594ceca8a500718
trabur <travis.burandt@gmal.com>
2021-01-23 23:51:46 -0600

    let others 2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import users from './v1/users.js'
-import vehicles from './v1/vehicles.js'
+import * as users from './v1/users.js'
+import * as vehicles from './v1/vehicles.js'
 
 export let istrav = {
-  users,
-  vehicles
+  account: {
+    users,
+  },
+  fleet: {
+    vehicles
+  },
+  store: {},
+  meta: {}
 }
