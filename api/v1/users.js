30a4f3f4ba8539844f0c2b2b0c8a96a1ff619e02
trabur <travis.burandt@gmal.com>
2021-01-23 23:47:49 -0600

    let others

--- api/v1/users.js
+++ api/v1/users.js
@@ -6,100 +6,97 @@ let host = 'http://localhost:3000'
 let endpoint = 'users'
 let token = ''
 
-function init (config) {
+export function init (config) {
   if (config.host) { host = config.host }
-  if (config.token) { host = config.token }
+  if (config.token) { token = config.token }
 }
 
-export default {
-  all: () => {
-    let that = this
-    return axios
-      .get(`${host}/api/${version}/${endpoint}`, {})
-      .then(function (response) {
-        console.log('all', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('all', endpoint, error)
-        return error
-      })
-  },
+export function all () {
+  let that = this
+  return axios
+    .get(`${host}/api/${version}/${endpoint}`, {})
+    .then(function (response) {
+      console.log('all', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('all', endpoint, error)
+      return error
+    })
+}
 
-  register: (params) => {
-    let that = this
-    return axios
-      .post(`${host}/api/${version}/${endpoint}`, {
-        params: params
-      })
-      .then(function (response) {
-        console.log('register', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('register', endpoint, error)
-        return error
-      })
-  },
+export function register (params) {
+  let that = this
+  return axios
+    .post(`${host}/api/${version}/${endpoint}`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log('register', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('register', endpoint, error)
+      return error
+    })
+}
 
-  login: (params) => {
-    let that = this
-    return axios
-      .post(`${host}/api/${version}/${endpoint}/login`, {
-        params: params
-      })
-      .then(function (response) {
-        console.log('login', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('login', endpoint, error)
-        return error
-      })
-  },
+export function login (params) {
+  let that = this
+  return axios
+    .post(`${host}/api/${version}/${endpoint}/login`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log('login', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('login', endpoint, error)
+      return error
+    })
+}
 
-  get: (id) => {
-    let that = this
-    return axios
-      .get(`${host}/api/${version}/${endpoint}/${id}`)
-      .then(function (response) {
-        console.log('get', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('get', endpoint, error)
-        return error
-      })
-  },
+export function get (id) {
+  let that = this
+  return axios
+    .get(`${host}/api/${version}/${endpoint}/${id}`)
+    .then(function (response) {
+      console.log('get', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('get', endpoint, error)
+      return error
+    })
+}
 
-  update: (id, params) => {
-    let that = this
-    return axios
-      .put(`${host}/api/${version}/${endpoint}/${id}`, {
-        params: params
-      })
-      .then(function (response) {
-        console.log('update', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('update', endpoint, error)
-        return error
-      })
-  },
+export function update (id, params) {
+  let that = this
+  return axios
+    .put(`${host}/api/${version}/${endpoint}/${id}`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log('update', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('update', endpoint, error)
+      return error
+    })
+}
   
-  remove: (id) => {
-    let that = this
-    return axios
-      .delete(`${host}/api/${version}/${endpoint}/${id}`)
-      .then(function (response) {
-        console.log('remove', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('remove', endpoint, error)
-        return error
-      })
-  }
+export function remove (id) {
+  let that = this
+  return axios
+    .delete(`${host}/api/${version}/${endpoint}/${id}`)
+    .then(function (response) {
+      console.log('remove', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('remove', endpoint, error)
+      return error
+    })
 }
-
--- api/v1/vehicles.js
+++ api/v1/vehicles.js
@@ -3,89 +3,87 @@ require('../../node_modules/axios/index.js')
 import { version } from './version.js'
 
 let host = 'http://localhost:3000'
-let endpoint = 'users'
+let endpoint = 'vehicles'
 let token = ''
 
-function init (config) {
+export function init (config) {
   if (config.host) { host = config.host }
   if (config.token) { host = config.token }
 }
 
-export default {
-  all: () => {
-    let that = this
-    return axios
-      .get(`${host}/api/${version}/${endpoint}`, {
-        headers: {'authorization': `Bearer ${token}`}
-      })
-      .then(function (response) {
-        console.log('all', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('all', endpoint, error)
-        return error
-      })
-  },
-  
-  save: (params) => {
-    let that = this
-    return axios
-      .post(`${host}/api/${version}/${endpoint}`, {
-        params: params
-      })
-      .then(function (response) {
-        console.log('save', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('save', endpoint, error)
-        return error
-      })
-  },
-  
-  get: (id) => {
-    let that = this
-    return axios
-      .get(`${host}/api/${version}/${endpoint}/${id}`)
-      .then(function (response) {
-        console.log('get', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('get', endpoint, error)
-        return error
-      })
-  },
+export function all () {
+  let that = this
+  return axios
+    .get(`${host}/api/${version}/${endpoint}`, {
+      headers: {'authorization': `Bearer ${token}`}
+    })
+    .then(function (response) {
+      console.log('all', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('all', endpoint, error)
+      return error
+    })
+}
 
-  update: (id, params) => {
-    let that = this
-    return axios
-      .put(`${host}/api/${version}/${endpoint}/${id}`, {
-        params: params
-      })
-      .then(function (response) {
-        console.log('update', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('update', endpoint, error)
-        return error
-      })
-  },
+export function save (params) {
+  let that = this
+  return axios
+    .post(`${host}/api/${version}/${endpoint}`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log('save', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('save', endpoint, error)
+      return error
+    })
+}
 
-  remove: (id) => {
-    let that = this
-    return axios
-      .delete(`${host}/api/${version}/${endpoint}/${id}`)
-      .then(function (response) {
-        console.log('remove', endpoint, response.data)
-        return response.data
-      })
-      .catch(function (error) {
-        console.log('remove', endpoint, error)
-        return error
-      })
-  }
+export function get (id) {
+  let that = this
+  return axios
+    .get(`${host}/api/${version}/${endpoint}/${id}`)
+    .then(function (response) {
+      console.log('get', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('get', endpoint, error)
+      return error
+    })
+}
+
+export function update (id, params) {
+  let that = this
+  return axios
+    .put(`${host}/api/${version}/${endpoint}/${id}`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log('update', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('update', endpoint, error)
+      return error
+    })
+}
+
+export function remove (id) {
+  let that = this
+  return axios
+    .delete(`${host}/api/${version}/${endpoint}/${id}`)
+    .then(function (response) {
+      console.log('remove', endpoint, response.data)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log('remove', endpoint, error)
+      return error
+    })
 }
 
