b7b86785e838972cd90329eaa3224c0f70082dd0
trabur <travis.burandt@gmal.com>
2021-01-25 23:01:49 -0600

    logging backup

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
+import * as sources from './v1/sources.js'
+import * as backup from './v1/backup.js'
 import * as logging from './v1/logging.js'
 import * as users from './v1/users.js'
 import * as vehicles from './v1/vehicles.js'
 
 export let istrav = {
   event: {
+    sources,
+    backup,
     logging
   },
   account: {
