1e3942207e08fe0ff59ec2272eefd717cf6f57a2
trabur <travis.burandt@gmal.com>
2021-01-26 14:04:23 -0600

    consume check

--- api/v1/sources.js
+++ api/v1/sources.js
@@ -23,3 +23,31 @@ export function publish (id, params) {
       return error
     })
 }
+
+export function consume (id, params) {
+  return axios
+    .post(`${host}/${version}/${endpoint}/consume/${id}`, {
+      params: params
+    })
+    .then(function (response) {
+      console.log(`consume ${version}/${endpoint}: ${JSON.stringify(response.data, null, 2)}`)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(`consume ${version}/${endpoint}: ${JSON.stringify(error, null, 2)}`)
+      return error
+    })
+}
+
+export function check (id) {
+  return axios
+    .get(`${host}/${version}/${endpoint}/check/${id}`)
+    .then(function (response) {
+      console.log(`check ${version}/${endpoint}: ${JSON.stringify(response.data, null, 2)}`)
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(`check ${version}/${endpoint}: ${JSON.stringify(error, null, 2)}`)
+      return error
+    })
+}
